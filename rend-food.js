2c146a6cc207bd6a5b2288ca62ae31b66152dfeb
Brevan Bender <bcaybender@gmail.com>
2021-10-15 13:38:16 -0700

    added footname class to span

--- rend-food.js
+++ rend-food.js
@@ -4,9 +4,10 @@ export function renderMe(food){
 
     const place = document.createElement('li');
     place.classList.add('food-card'); 
-    
+       
     const name = document.createElement('span');
     name.textContent = food.name;
+    name.classList.add('food-name');
     
     const img = document.createElement('img');
     img.src = food.img;
